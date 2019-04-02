25b62dd5f462e48506b7505365677e57a98477b2
robitalhazmi <robitalhazmi@gmail.com>
2019-04-02 14:56:27 +0700

    fix bug on investor testing

--- routes/auth.js
+++ routes/auth.js
@@ -4,9 +4,18 @@ import { User, createUser, getUserByEmail, comparePassword, getUserByID } from '
 import passport from 'passport';
 import nodemailer from 'nodemailer';
 import randomstring from 'randomstring';
+import path from 'path';
+import fs from 'fs-extra';
+import ejs from 'ejs';
 
 const router = express.Router();
 const LocalStrategy = require('passport-local').Strategy;
+
+const compile = async function (templateName, data) {
+    const filePath = path.join(__dirname, '../storage/email/', `${templateName}.ejs`);
+    const html = await fs.readFile(filePath, 'utf-8');
+    return ejs.render(html, data);
+};
 
 router.get('/', function (req, res) {
     res.redirect('/auth/login');
@@ -48,7 +57,7 @@ router.post('/register', notLoggedIn, function (req, res) {
         return ;
     }
     else {
-        getUserByEmail(email, function (err, user) {
+        getUserByEmail(email, async function (err, user) {
             if (err) {
                 error_message = "Terjadi kesalahan"; 
                 req.flash('error_message', error_message);
@@ -62,6 +71,7 @@ router.post('/register', notLoggedIn, function (req, res) {
                 return ;
             }
             else {
+                
                 let transporter = nodemailer.createTransport({
                     host: 'smtp.gmail.com',
                     port: 465,
@@ -71,11 +81,15 @@ router.post('/register', notLoggedIn, function (req, res) {
                         pass: 'investani2019'
                     }
                 });
+                let data =  {
+                    secretToken: secretToken
+                }
+                const content = await compile('confirmation', data);
                 let mailOptions = {
                     from: '"Investani" <investaninx@gmail.com>',
                     to: email,
                     subject: "Konfirmasi Email Pendaftaran",
-                    html: `<!DOCTYPE > <html> <head> <title>Internal_email-29</title> <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> <meta name="viewport" content="width=device-width, initial-scale=1.0" /> <style type="text/css"> * { -ms-text-size-adjust:100%; -webkit-text-size-adjust:none; -webkit-text-resize:100%; text-resize:100%; } a{ outline:none; color:#40aceb; text-decoration:underline; } a:hover{text-decoration:none !important;} .nav a:hover{text-decoration:underline !important;} .title a:hover{text-decoration:underline !important;} .title-2 a:hover{text-decoration:underline !important;} .btn:hover{opacity:0.8;} .btn a:hover{text-decoration:none !important;} .btn{ -webkit-transition:all 0.3s ease; -moz-transition:all 0.3s ease; -ms-transition:all 0.3s ease; transition:all 0.3s ease; } table td {border-collapse: collapse !important;} .ExternalClass, .ExternalClass a, .ExternalClass span, .ExternalClass b, .ExternalClass br, .ExternalClass p, .ExternalClass div{line-height:inherit;} @media only screen and (max-width:500px) { table[class="flexible"]{width:100% !important;} table[class="center"]{ float:none !important; margin:0 auto !important; } *[class="hide"]{ display:none !important; width:0 !important; height:0 !important; padding:0 !important; font-size:0 !important; line-height:0 !important; } td[class="img-flex"] img{ width:100% !important; height:auto !important; } td[class="aligncenter"]{text-align:center !important;} th[class="flex"]{ display:block !important; width:100% !important; } td[class="wrapper"]{padding:0 !important;} td[class="holder"]{padding:30px 15px 20px !important;} td[class="nav"]{ padding:20px 0 0 !important; text-align:center !important; } td[class="h-auto"]{height:auto !important;} td[class="description"]{padding:30px 20px !important;} td[class="i-120"] img{ width:120px !important; height:auto !important; } td[class="footer"]{padding:5px 20px 20px !important;} td[class="footer"] td[class="aligncenter"]{ line-height:25px !important; padding:20px 0 0 !important; } tr[class="table-holder"]{ display:table !important; width:100% !important; } th[class="thead"]{display:table-header-group !important; width:100% !important;} th[class="tfoot"]{display:table-footer-group !important; width:100% !important;} } </style> </head> <body style="margin:0; padding:0;" bgcolor="#eaeced"> <table style="min-width:320px;" width="100%" cellspacing="0" cellpadding="0" bgcolor="#eaeced"> <!-- fix for gmail --> <tr> <td class="hide"> <table width="600" cellpadding="0" cellspacing="0" style="width:600px !important;"> <tr> <td style="min-width:600px; font-size:0; line-height:0;">&nbsp;</td> </tr> </table> </td> </tr> <tr> <td class="wrapper" style="padding:0 10px;"> <!-- module 3 --> <table data-module="module-3" data-thumb="thumbnails/03.png" width="100%" cellpadding="0" cellspacing="0"> <tr> <td data-bgcolor="bg-module" bgcolor="#eaeced"> <table class="flexible" width="600" align="center" style="margin:0 auto;" cellpadding="0" cellspacing="0"> <tr> <td class="img-flex"><img src="images/img-02.jpg" style="vertical-align:top;" width="600" height="249" alt="" /></td> </tr> <tr> <td data-bgcolor="bg-block" class="holder" style="padding:50px 50px 50px;" bgcolor="#f9f9f9"> <table width="100%" cellpadding="0" cellspacing="0"> <tr> <td data-color="title" data-size="size title" data-min="20" data-max="40" data-link-color="link title color" data-link-style="text-decoration:none; color:#292c34;" class="title" align="center" style="font:30px/33px Arial, Helvetica, sans-serif; color:#292c34; padding:0 0 24px;"> Investani </td> </tr> <tr> <td data-color="text" data-size="size text" data-min="10" data-max="26" data-link-color="link text color" data-link-style="font-weight:bold; text-decoration:underline; color:#40aceb;" align="center" style="font:12px Arial, Helvetica, sans-serif; color:#888; padding:0 0 21px;"> Email ini dibuat secara otomatis. Mohon tidak mengirimkan balasan ke email ini. Bila membutuhkan bantuan, hubungi ... </td> </tr> <tr> <td data-color="text" data-size="size text" data-min="10" data-max="26" data-link-color="link text color" data-link-style="font-weight:bold; text-decoration:underline; color:#40aceb;" align="left" style="font:bold 1.5em Arial, Helvetica, sans-serif; color:#292c34; padding:0 0 23px;"> Hi, </td> </tr> <tr> <td data-color="text" data-size="size text" data-min="10" data-max="26" data-link-color="link text color" data-link-style="font-weight:bold; text-decoration:underline; color:#40aceb;" align="left" style="font:16px Arial, Helvetica, sans-serif; color:#292c34; padding:0 0 21px;"> Terima kasih telah mendaftarkan email anda di Investani. <br> Tekan tombol di bawah ini untuk aktivasi email Anda. </td> </tr> <tr> <td style="padding:0 0 20px;"> <table width="134" align="left" style="margin:0 auto;" cellpadding="0" cellspacing="0"> <tr> <td data-bgcolor="bg-button" data-size="size button" data-min="10" data-max="16" class="btn" align="center" style="font:12px/14px Arial, Helvetica, sans-serif; color:#f8f9fb; text-transform:uppercase; mso-padding-alt:12px 10px 10px; border-radius:2px;" bgcolor="#40aceb"> <a target="_blank" style="text-decoration:none; color:#f8f9fb; display:block; padding:12px 10px 10px;" href="http://127.0.0.1:3000/activation/${secretToken}">Klik di sini</a> </td> </tr> </table> </td> </tr> <tr> <td data-color="text" data-size="size text" data-min="10" data-max="26" data-link-color="link text color" data-link-style="font-weight:bold; text-decoration:underline; color:#40aceb;" align="left" style="font:16px Arial, Helvetica, sans-serif; color:#888; padding:0 0 21px;"> <b>Mengapa harus mengaktivasi email?</b> <br> Ini adalah bentuk keamanan sistem kami. Jika email Anda tidak diverifikasi, maka Anda tidak dapat melanjutkan pendaftaran. </td> </tr> </table> </td> </tr> <tr><td height="28"></td></tr> </table> </td> </tr> </table> </td> </tr> <!-- fix for gmail --> <tr> <td style="line-height:0;"><div style="display:none; white-space:nowrap; font:15px/1px courier;">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</div></td> </tr> </table> </body></html>`
+                    html: content
                 };
         
                 transporter.sendMail(mailOptions, (error, info) => {
